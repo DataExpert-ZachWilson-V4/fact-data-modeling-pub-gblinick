c5064c4c811e8e92e3b0973a08339fac595ecf17
Gideon Blinick <gideon.blinick@gmail.com>
2024-05-21 23:03:28 -0400

    q8

---
+++ submission/query_8.sql
@@ -0,0 +1,36 @@
+INSERT INTO gblinick.host_activity_reduced
+WITH
+  yesterday AS (
+    SELECT
+      *
+    FROM
+      gblinick.host_activity_reduced
+    WHERE
+      month_start = '2023-08-01'
+  ),
+  today AS (
+    SELECT
+      *
+    FROM
+      gblinick.daily_web_metrics
+    WHERE
+      DATE = DATE('2023-08-02')
+  )
+SELECT
+  COALESCE(t.host, y.host) AS host,
+  COALESCE(t.metric_name, y.metric_name) AS metric_name,
+  COALESCE(
+    y.metric_array,
+    REPEAT(
+      NULL,
+      CAST(
+        DATE_DIFF('day', DATE('2023-08-01'), t.date) AS INTEGER
+      )
+    )
+  ) || ARRAY[t.metric_value] AS metric_array,
+  '2023-08-01' AS month_start
+FROM
+  today t
+  FULL OUTER JOIN yesterday y 
+  ON t.host = y.host
+  AND t.metric_name = y.metric_name

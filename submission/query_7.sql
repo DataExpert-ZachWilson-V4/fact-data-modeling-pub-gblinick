b460b5e3a1942c75272cbdade5a8df3444856b30
Gideon Blinick <gideon.blinick@gmail.com>
2024-05-21 22:15:23 -0400

    q7

---
+++ submission/query_7.sql
@@ -0,0 +1,12 @@
+CREATE TABLE gblinick.host_activity_reduced (
+  host VARCHAR,
+  metric_name VARCHAR,
+  metric_array ARRAY(INTEGER),
+  month_start VARCHAR
+)
+WITH
+  (
+    FORMAT = 'PARQUET',
+    partitioning = ARRAY['metric_name', 'month_start']
+  )
+
